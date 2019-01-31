3a622320163931ab86646b086da9b95774836425
ilhoon lee <rival6821@naver.com>
2019-01-31 23:18:56 +0900

    머터리얼 디자인 추가해 보려고 고민중

--- pront-end/src/components/Login/Login.js
+++ pront-end/src/components/Login/Login.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from './login.scss';
 import classNames from 'classnames/bind';
+import Input from '@material-ui/core/Input';
+import Grid from '@material-ui/core/Grid';
+
 const cx = classNames.bind(styles);
 
 const Login = () => {
     return (
-        <div className={cx('login-wrap')}>
-                
-        </div>
+        <Grid container spacing={24} className={cx('login-wrap')}>
+            <Input type="text" autoFocus placeholder="이메일" className={cx('input','email-input')}/>
+            <Input type="password" placeholder="비밀번호" className={cx('input','password-input')}/>
+            <button>로그인</button>
+        </Grid>
     );
 };
 
--- pront-end/src/pages/Login.js
+++ pront-end/src/pages/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import LoginComponent from '../components/Login/Login';
 
 const Login = () => {
     return (
         <div>
-            Login Page
+            <LoginComponent/>
         </div>
     );
 };
