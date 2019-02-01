8b32b8538e34702008193a627408511ae8c2f28e
ilhoon lee <rival6821@naver.com>
2019-02-01 11:19:09 +0900

    로그인 페이지 디자인

--- pront-end/src/components/Login/Login.js
+++ pront-end/src/components/Login/Login.js
@@ -2,18 +2,40 @@ import React from 'react';
 import styles from './login.scss';
 import classNames from 'classnames/bind';
 import Input from '@material-ui/core/Input';
+import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+const m_styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
 
 const cx = classNames.bind(styles);
 
-const Login = () => {
+function Login(props){
+    const { classes } = props;
     return (
-        <Grid container spacing={24} className={cx('login-wrap')}>
-            <Input type="text" autoFocus placeholder="이메일" className={cx('input','email-input')}/>
-            <Input type="password" placeholder="비밀번호" className={cx('input','password-input')}/>
-            <button>로그인</button>
-        </Grid>
+        <div className={classes.root}>
+            <Grid container className={cx('login-wrap')}>
+                <Grid item xs={12}>
+                    <Input type="text" autoFocus placeholder="이메일" className={cx('input','email-input')}/>
+                </Grid>
+                <Grid item xs={12}>
+                    <Input type="password" placeholder="비밀번호" className={cx('input','password-input')}/>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" size="large" className={classes.button}>
+                        로그인
+                    </Button>
+                </Grid>
+            </Grid>
+        </div>
     );
 };
 
-export default Login;
+export default withStyles(m_styles)(Login);
