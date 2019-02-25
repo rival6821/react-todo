5f7ebbd5b71435ddfb20be397621899d1b424e41
ilhoon lee <rival6821@naver.com>
2019-02-25 22:14:08 +0900

    리액트 빌드 테스트

---
+++ pront-end/build/precache-manifest.f43b5ca2f2bb7e55d5a3642f4a2f9d36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d6586e0ce6c3c8fc0943",
+    "url": "/static/js/main.d6586e0c.chunk.js"
+  },
+  {
+    "revision": "bd190ee77fcd8918aee7",
+    "url": "/static/js/2.bd190ee7.chunk.js"
+  },
+  {
+    "revision": "d6586e0ce6c3c8fc0943",
+    "url": "/static/css/main.b1bd5cb2.chunk.css"
+  },
+  {
+    "revision": "d1ede888b1046906f5badb0553405c48",
+    "url": "/index.html"
+  }
+];
--- pront-end/build/service-worker.js
+++ pront-end/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c27497477d5b1c8aa080b165688eadc.js"
+  "/precache-manifest.f43b5ca2f2bb7e55d5a3642f4a2f9d36.js"
 );
 
 workbox.clientsClaim();
